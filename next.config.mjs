c4ec67ac4a7b60c1d9d4251217d6b23544b8cb04
ywcheng1207 <ywcheng1207.git@gmail.com>
2024-03-01 23:18:47 +0800

    style

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   eslint: {
     // 忽略在建構時的 ESLint 錯誤
-    ignoreDuringBuilds: true,
-  },
-};
+    ignoreDuringBuilds: true
+  }
+}
 
-export default nextConfig;
+export default nextConfig
