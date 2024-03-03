5aa7db21d0a9a2ebab633ff1fa6b7ae6e0e1e0d9
ywcheng1207 <ywcheng1207.git@gmail.com>
2024-03-03 19:50:51 +0800

    feat: add classManage, class... page

---
+++ src/app/class/[class_id]/page.js
@@ -0,0 +1,9 @@
+export default function ClassesPage({ params }) {
+  const classId = params.class_id
+
+  return (
+    <div>
+      <div className="text-[50px] text-[#66BFFF]">課程編號 {classId} 頁面施工中...</div>
+    </div>
+  )
+}
--- src/app/teacher/[teacher_id]/classManage/page.js
+++ src/app/teacher/[teacher_id]/classManage/page.js
@@ -3,10 +3,13 @@
 //
 import Image from 'next/image'
 import { Button, Select, Modal, Form, Input } from 'antd'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 //
 import iconEdit from '@/assets/icon-edit.svg'
+import iconArrow from '@/assets/icon-arrow.svg'
+import iconRunningMan from '@/assets/icon-running-man.svg'
+import iconDelete from '@/assets/icon-delete.svg'
 
 const dummyClassesData = [
   {
@@ -14,9 +17,9 @@ const dummyClassesData = [
     isBooked: 0,
     isCompleted: 0,
     length: 60,
-    dateTimeRange: '2024-03-02 19:00-20:00',
+    dateTimeRange: '2024-03-05 19:00-20:00',
     name: '試試看保證不虧',
-    link: 'https://tw.yahoo.com/?p=us',
+    link: '/class/1',
     studentId: null,
     teacherId: 21,
     categoryId: 1,
@@ -28,9 +31,9 @@ const dummyClassesData = [
     isBooked: 0,
     isCompleted: 0,
     length: 30,
-    dateTimeRange: '2024-03-02 20:00-20:30',
+    dateTimeRange: '2024-03-05 20:00-20:30',
     name: '試了你就知道會虧',
-    link: 'https://www.youtube.com/',
+    link: '/class/2',
     studentId: null,
     teacherId: 21,
     categoryId: 1,
@@ -42,9 +45,9 @@ const dummyClassesData = [
     isBooked: 0,
     isCompleted: 0,
     length: 30,
-    dateTimeRange: '2024-03-02 20:00-20:30',
+    dateTimeRange: '2024-03-05 20:30-21:00',
     name: '沒錢還想上課',
-    link: 'https://www.facebook.com/',
+    link: '/class/3',
     studentId: null,
     teacherId: 21,
     categoryId: 1,
@@ -56,9 +59,9 @@ const dummyClassesData = [
     isBooked: 0,
     isCompleted: 0,
     length: 60,
-    dateTimeRange: '2024-03-03 19:00-20:00',
+    dateTimeRange: '2024-03-06 19:00-20:00',
     name: '試試看保證不虧',
-    link: 'https://tw.yahoo.com/?p=us',
+    link: '/class/6',
     studentId: null,
     teacherId: 21,
     categoryId: 1,
@@ -70,9 +73,9 @@ const dummyClassesData = [
     isBooked: 0,
     isCompleted: 0,
     length: 30,
-    dateTimeRange: '2024-03-03 20:00-20:30',
+    dateTimeRange: '2024-03-06 20:00-20:30',
     name: '試了你就知道會虧',
-    link: 'https://www.youtube.com/',
+    link: '/class/7',
     studentId: null,
     teacherId: 21,
     categoryId: 1,
@@ -84,9 +87,9 @@ const dummyClassesData = [
     isBooked: 0,
     isCompleted: 0,
     length: 30,
-    dateTimeRange: '2024-03-05 20:00-20:30',
+    dateTimeRange: '2024-03-07 20:00-20:30',
     name: '試了你就知道會虧',
-    link: 'https://www.youtube.com/',
+    link: '/class/8',
     studentId: null,
     teacherId: 21,
     categoryId: 1,
@@ -99,15 +102,23 @@ const dummyClassesData = [
 export default function ClassManage({ params }) {
   const teacherId = params.teacher_id
   const [classData, setClassData] = useState([])
-  const [fixClassData, setFixClassData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [currentTime, setCurrentTime] = useState(getNextTwoWeeksDates()[0])
-  const currentClassData = extractDateTimeRangesCurrentData(fixClassData)
+  // 桌機版tab位置捕捉
+  const scrollContainer = useRef(null)
+  // 編輯日期的時間選項裡面要標註有課程的項目
+  const dateSelection = extractDatesFromClassData(classData)
+  // modal內的時間選項
+  const currentClassData = extractDateTimeRangesCurrentData(classData)
   const timeSelection = formatTimeSelection(currentTime)
   const finalSelection = findUniqueTimeSelection(currentClassData, timeSelection)
 
   const handleSelectTime = (e) => {
     setCurrentTime(e)
+  }
+
+  const handleDeleteClass = (id) => {
+    setClassData(pre => pre.filter(ele => ele.id !== id))
   }
 
   const handleEditClass = (e, type) => {
@@ -131,27 +142,42 @@ export default function ClassManage({ params }) {
     }
   }
 
+  const scrollLeft = () => {
+    if (scrollContainer.current) {
+      // 向左滾動
+      scrollContainer.current.scrollBy({ left: -300, behavior: 'smooth' })
+    }
+  }
+
+  const scrollRight = () => {
+    if (scrollContainer.current) {
+      // 向右滾動
+      scrollContainer.current.scrollBy({ left: 300, behavior: 'smooth' })
+    }
+  }
+
   useEffect(() => {
     setIsLoading(true)
-    setFixClassData(dummyClassesData)
-    setClassData(dummyClassesData.filter(ele => ele.dateTimeRange.split(' ')[0] === currentTime))
+    setClassData(dummyClassesData)
     setIsLoading(false)
-  }, [currentTime])
+  }, [])
 
   const content = () => {
+    const data = classData.filter(ele => ele.dateTimeRange.split(' ')[0] === currentTime)
     if (isLoading) {
       return (
         <div>載入中...</div>
       )
     }
-    if (classData.length > 0 && !isLoading) {
+    if (data.length > 0 && !isLoading) {
       return (
-        <div className='flex flex-col gap-3' >
-          {classData.map(ele =>
+        <div className='flex flex-col gap-3 md:flex-row md:flex-wrap' >
+          {data.map(ele =>
             <ClassCard
               key={ele.id}
               info={ele}
               onEdit={handleEditClass}
+              onDeleteClass={handleDeleteClass}
               finalSelection={finalSelection}
               currentClassData={currentClassData}
             />)
@@ -163,34 +189,72 @@ export default function ClassManage({ params }) {
   }
 
   return (
-    <div className="w-full flex flex-col gap-3">
-      <div className='w-full bg-[#CCC] text-center py-2 rounded-sm'>課程列表</div>
-      <h6>請選擇要編輯的日期</h6>
-      <Select onChange={handleSelectTime} placeholder='日期' defaultValue={currentTime}>
-        {getNextTwoWeeksDates().map(ele => <Select.Option value={ele} key={ele}>
-          <div className='flex gap-2'>
-            <div>
-              {ele}
-            </div>
-            {
-              extractDatesFromClassData(fixClassData).includes(ele) &&
-              <div className='text-green-500'>&#10003;</div>
-            }
+    <>
+      {/* title */}
+      <div className='w-full bg-[#CCC] text-[#fff] text-center py-2 rounded-sm mb-3'>課程列表</div>
+
+      {/* desktop */}
+      <div className='hidden md:block'>
+        <div className='flex items-center'>
+          <div onClick={scrollLeft} className='h-[70px] min-w-[50px] flex justify-center items-center cursor-pointer hover:bg-slate-100'>
+            <Image src={iconArrow} alt='arrow' className='rotate-180' />
           </div>
-        </Select.Option>)}
-      </Select>
-      <EditBtn type='create' onEdit={handleEditClass} finalSelection={finalSelection} currentClassData={currentClassData} />
-      {content()}
-    </div>
+          <div ref={scrollContainer} className='flex overflow-hidden'>
+            {getNextTwoWeeksDates().map(ele =>
+              <DeskTabCard
+                key={ele}
+                date={ele}
+                currentTime={currentTime}
+                onSelectTime={handleSelectTime}
+                dateSelection={dateSelection}
+              />
+            )}
+          </div>
+          <div onClick={scrollRight} className='h-[70px] min-w-[50px] flex justify-center items-center cursor-pointer hover:bg-slate-100'>
+            <Image src={iconArrow} alt='arrow' />
+          </div>
+        </div>
+        <div className='bg-[#CCC] w-full min-h-[500px] rounded-md p-3 flex flex-col justify-between'>
+          {content()}
+          <div className='flex justify-end'>
+            <EditBtn type='create' onEdit={handleEditClass} finalSelection={finalSelection} currentClassData={currentClassData} />
+          </div>
+        </div>
+      </div>
+
+      {/* mobile */}
+      <div className="w-full flex flex-col gap-3 md:hidden">
+        <h6>請選擇要編輯的日期</h6>
+        <Select onChange={handleSelectTime} placeholder='日期' value={currentTime}>
+          {getNextTwoWeeksDates().map(ele =>
+            <Select.Option value={ele} key={ele}>
+              <div className='flex gap-2'>
+                <div>
+                  {ele}
+                </div>
+                {
+                  dateSelection.includes(ele) &&
+                  <div className='text-green-500'>&#10003;</div>
+                }
+              </div>
+            </Select.Option>)
+          }
+        </Select>
+        <EditBtn type='create' onEdit={handleEditClass} finalSelection={finalSelection} currentClassData={currentClassData} />
+        {content()}
+      </div>
+    </>
+
   )
 }
 
-const ClassCard = ({ info, onEdit, finalSelection, currentClassData }) => {
+// --------------- children components ---------------
+const ClassCard = ({ info, onEdit, onDeleteClass, finalSelection, currentClassData }) => {
   return (
     <div
-      className='w-full min-h-[150px] border-[1px] border-solid border-[#CCC] rounded-[3px] p-3 flex flex-col gap-2'
+      className='w-full min-h-[150px] border-[1px] border-solid border-[#CCC] rounded-[3px] p-3 flex flex-col gap-2 md:bg-[#FFF] md:max-w-[230px] md:rounded-lg md:text-[15px]'
     >
-      <div className='flex'>
+      <div className='flex md:flex-col'>
         <h1>課程名稱：</h1>
         <h1>{info.name}</h1>
       </div>
@@ -198,7 +262,7 @@ const ClassCard = ({ info, onEdit, finalSelection, currentClassData }) => {
         <h1>課程分類：</h1>
         <h1>{info.categoryId}</h1>
       </div>
-      <div className='flex'>
+      <div className='flex md:flex-col'>
         <h1>上課時間：</h1>
         <h1>{info.dateTimeRange}</h1>
       </div>
@@ -206,10 +270,11 @@ const ClassCard = ({ info, onEdit, finalSelection, currentClassData }) => {
         <h1>上課時長：</h1>
         <h1>{info.length}</h1>
       </div>
-      <div className='flex'>
+      <div className='flex md:flex-col'>
         <a href={info.link} target='_blank' className='text-[#66BFFF] hover:opacity-70'>課程連結</a>
       </div>
-      <div className='flex justify-end'>
+      <div className='flex justify-between'>
+        <Image src={iconDelete} alt='delete' className='cursor-pointer' onClick={() => onDeleteClass(info.id)}/>
         <EditBtn type='edit' info={info} onEdit={onEdit} finalSelection={finalSelection} currentClassData={currentClassData} />
       </div>
     </div>
@@ -221,7 +286,7 @@ const EditBtn = ({ type, info, onEdit, finalSelection, currentClassData }) => {
   const [form] = Form.useForm()
   const showModal = () => {
     setIsModalOpen(!isModalOpen)
-    form.resetFields()
+    if (type === 'create') form.resetFields()
   }
 
   const handleModalSubmit = (e) => {
@@ -328,6 +393,29 @@ const EditBtn = ({ type, info, onEdit, finalSelection, currentClassData }) => {
   )
 }
 
+const DeskTabCard = ({ date, currentTime, onSelectTime, dateSelection }) => {
+  return (
+    <>
+      {date === currentTime
+        ? <div
+          className='min-w-[160px] h-[80px] bg-[#CCC] text-[#FFF] flex justify-center items-center rounded-tl-md rounded-tr-md relative'
+        >
+          {date}{dateSelection.includes(date) && <Image src={iconRunningMan} alt='running' className='absolute top-0 right-0' />}
+        </div>
+        : <div
+          className='min-w-[160px] h-[80px] bg-[#fff] flex justify-center items-center cursor-pointer hover:opacity-40 relative'
+          onClick={() => onSelectTime(date)}
+        >
+          {date}{dateSelection.includes(date) && <Image src={iconRunningMan} alt='running' className='absolute top-0 right-0' />}
+        </div>
+      }
+    </>
+  )
+}
+
+// --------------- functions ---------------
+
+//
 function getNextTwoWeeksDates() {
   const result = []
   const currentDate = new Date() // 獲取當前日期
@@ -341,7 +429,7 @@ function getNextTwoWeeksDates() {
 
   return result
 }
-
+//
 function extractDatesFromClassData(classData) {
   // 使用 Set 確保日期不重複
   const dateSet = new Set()
@@ -357,7 +445,7 @@ function extractDatesFromClassData(classData) {
   // 將 Set 轉換為陣列並返回
   return Array.from(dateSet)
 }
-
+//
 function generateRandomString() {
   // 定義可能的字符集合
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -371,16 +459,16 @@ function generateRandomString() {
 
   return result
 }
-
+//
 function extractDateTimeRangesCurrentData(data) {
   return data.map(item => item.dateTimeRange)
 }
-
+//
 function findUniqueTimeSelection(arrayA, arrayB) {
   const filteredArrayB = arrayB.filter(item => !arrayA.includes(item))
   return filteredArrayB
 }
-/// =================================================
+//
 function parseDateTimeRange(dateTimeRange) {
   const [datePart, timePart] = dateTimeRange.split(' ')
   const [startTime, endTime] = timePart.split('-')
@@ -388,7 +476,7 @@ function parseDateTimeRange(dateTimeRange) {
   const endDate = new Date(`${datePart} ${endTime}`)
   return [startDate, endDate]
 }
-
+//
 function isOverlapping(interval1, interval2) {
   const [start1, end1] = parseDateTimeRange(interval1)
   const [start2, end2] = parseDateTimeRange(interval2)
@@ -399,45 +487,27 @@ function isOverlapping(interval1, interval2) {
   }
   return interval1 // 回傳符合條件的時間區間資料
 }
-
-// 假設這是您的10筆時間區間
-// const intervals1 = [
-//   '2024/02/26 18:00-18:30',
-//   '2024/02/26 19:00-19:30',
-//   '2024/02/26 21:00-21:30'
-//   // 添加其他8筆時間區間
-// ]
-
-// 您要比對的時間區間
-// const interval2 = '2024/02/26 18:00-19:00'
-
-// 逐一比對
-// const overlappingResults = intervals1.map(interval1 => isOverlapping(interval1, interval2))
-
-// console.log('測試區', overlappingResults)
-
+//
 const defaultTimeSelection30 = [
   '18:00-18:30', '18:30-19:00', '19:00-19:30', '19:30-20:00',
   '20:00-20:30', '20:30-21:00'
 ]
-
 const defaultTimeSelection60 = [
   '18:00-19:00', '19:00-20:00', '20:00-21:00'
 ]
-
 const formatTimeSelection = (date) => {
   const data1 = defaultTimeSelection30.map(ele => `${date} ${ele}`)
   const data2 = defaultTimeSelection60.map(ele => `${date} ${ele}`)
   return [...data1, ...data2]
 }
-
 const createValidator = (intervals1) => {
   return (_, value) => {
     const overlappingResults = intervals1
       .map(interval1 => isOverlapping(interval1, value))
       .filter(result => result !== false)
+    if (overlappingResults.includes(value)) return Promise.resolve()
     if (overlappingResults.length > 0) {
-      return Promise.reject(new Error(`時段重複(如欲設定該時段請先刪除重複的時程)：${overlappingResults.join('　|　')}`))
+      return Promise.reject(new Error(`如欲設定該時段，請先刪除重複的時程：${overlappingResults.join('　|　')}`))
     }
     return Promise.resolve()
   }
